5294698451bcd61e8c1ad4ee8b6425eb866601bf
Ali Bdeir <ali@bdeir.org>
2021-04-18 04:29:29 +0300

    Attributions, github link, style

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,13 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+        <footer>
+            <a href="https://github.com/AyBee0/Baydati">Github</a>
+            <br />
+            <a href="https://www.svgbackgrounds.com/">Background by svgbackgrounds.com</a>
+        </footer>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
